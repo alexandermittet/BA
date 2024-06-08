4f5fd02f84603c2bb37f5fe3d051ce08b4221cc9
Alexander Mittet <alexandermittet@gmail.com>
2024-06-08 14:11:16 +0200

    utils

--- utils/laplacian_filter.py
+++ utils/laplacian_filter.py
@@ -1,13 +1,20 @@
 import torch
 from torch import nn
 
+
 def laplacian_sharpness(image):
     # Convert image to grayscale if it's not already
     if image.shape[1] > 1:
-    image = image.mean(dim=1, keepdim=True)
+        image = image.mean(dim=1, keepdim=True)
 
-    # Define Laplacian filter
-    filter = torch.tensor([[0, 1, 0], [1, -4, 1], [0, 1, 0]], dtype=torch.float32).unsqueeze(0).unsqueeze(0)
+        # Define Laplacian filter
+        filter = (
+            torch.tensor(
+                [[0, 1, 0], [1, -4, 1], [0, 1, 0]], dtype=torch.float32
+            )
+            .unsqueeze(0)
+            .unsqueeze(0)
+        )
 
     # Apply filter and calculate mean absolute value
     laplacian = nn.functional.conv2d(image, filter, bias=False)
